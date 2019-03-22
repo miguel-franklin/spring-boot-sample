39bec87614dd93f2ce82f45ddcf4bed7732899b0
Miguel Franklin <miguel.franklin-mariano-filho@hpe.com>
2019-03-22 03:55:17 -0300

    jenkins file

--- src/test/java/nl/revolution/example/SampleApplicationTest.java
+++ src/test/java/nl/revolution/example/SampleApplicationTest.java
@@ -1,5 +1,6 @@
 package nl.revolution.example;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ public class SampleApplicationTest {
 
 	@Test
 	public void contextLoads() {
+		Assert.assertTrue(false);
 	}
 
 }
